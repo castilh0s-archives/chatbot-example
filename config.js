4fd63ad7905cc7b9e55646bce74dd48d750ae982
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-02 10:53:48 -0300

    :tada: Commit inicial

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  SERVER_URL: undefined,
+  FB_APP_SECRET: undefined,
+  GOOGLE_PRIVATE_KEY: undefined,
+  GOOGLE_CLIENT_EMAIL: undefined,
+  DF_LANGUAGE_CODE: undefined,
+  GOOGLE_PROJECT_ID: undefined,
+  FB_VERIFY_TOKEN: undefined,
+  FB_PAGE_TOKEN: undefined
+};
